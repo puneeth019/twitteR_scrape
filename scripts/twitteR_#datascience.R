5dcc1f5629723a5736afee25009c4c2f285d1eff
Puneeth G <puneeth019@gmail.com>
2017-02-14 21:55:03 +0530

    Update twitteR_#datascience.R

--- scripts/twitteR_#datascience.R
+++ scripts/twitteR_#datascience.R
@@ -40,7 +40,7 @@ dm <- tdm %>%
     sort(rowSums(.), decreasing = TRUE) %>%
     data.frame(word = names(.), freq = .)
 
-png(paste0(WorkDir,"plots/", search_string,".png"), height = 2000, width = 2000)
+png(paste0(WorkDir,"word-clouds/", search_string,".png"), height = 2000, width = 2000)
 wordcloud(dm$word, dm$freq, scale=c(12,2), random.order = FALSE,
           colors= brewer.pal(5, "Set1"), max.words = 800, min.freq = 5,
           rot.per=.15)
